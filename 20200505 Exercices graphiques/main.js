ee28153599d365f12f7f293a8f6e02cd14a3a8d0
Wara-nyu <w.bounkeua@gmail.com>
2020-05-18 16:09:56 +0200

    apparition du vaisseau principal

--- 20200505 Exercices graphiques/main.js
+++ 20200505 Exercices graphiques/main.js
@@ -2,17 +2,17 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 let count = 0;
 let starArray = [];
+let ship; 
 
 function setup() {
   createCanvas(w, h);
   for (i = 0 ; i < 40 ; i++) {
 	  starArray.push(new Star);
   }
+  ship = new Ship();
   //middle()
 }
 
-x = 0;
-y = 100;
 
 function draw() {
   background(0);
@@ -26,17 +26,4 @@ function middle() {
   x = width/2;
   y = height/2;
 }
-
-function move() {
-  if((x+300) < width){
-    x += 3;
-  } else {
-    x += 0;
-    starArray.forEach(function(item){
-      item.sMove();
-    })
-  }
-}
-
-
 
--- 20200505 Exercices graphiques/ship.js
+++ 20200505 Exercices graphiques/ship.js
@@ -1,32 +1,36 @@
 class Ship {
 
-  n = 10
+  constructor() {
+    this.x = 0;
+    this.y = 100;
+    this.n = 10;
+  }
 
-  function principalShip() {
+  principalShip() {
     fill(220);
-    triangle(x, y, x, y + n*10, x + n*13, y +n*10);
-    triangle(x, y + n*20, x, y + n*30, x + n*13, y + n*20);
-    triangle(x, y + n*5, x, y + n*25, x + n*30, y + n*15);
+    triangle(this.x, this.y, this.x, this.y + this.n*10, this.x + this.n*13, this.y + this.n*10);
+    triangle(this.x, this.y + this.n*20, this.x, this.y + this.n*30, this.x + this.n*13, this.y + this.n*20);
+    triangle(this.x, this.y + this.n*5, this.x, this.y + this.n*25, this.x + this.n*30, this.y + this.n*15);
   }
 
-  function sideShip() {
-    n = random(5);//trouver comment donner un interval pour evité le 0
-    x -= n;// |!| si speed est positif x += n si speed est negatif à implémenter
-    ship();
+  sideShip() {
+    this.n = random(5);//trouver comment donner un interval pour evité le 0
+    this.x -= this.n;// |!| si speed est positif this.x += this.n si speed est negatif à implémenter
+    principalShip();
   }
 
-  function movePrincipalS() {
+  movePrincipalS() {
     if((x+300) < width){
-      x += 3;
+      this.x += 3;
     } else {
-      x += 0;
+      this.x += 0;
     }
   }
 
-  function moveSideS() {
-    speed = random (10);//speed = 0 N'est pas un soucis
-    x += speed;
+  moveSideS() {
+    let speed = random (10);//speed = 0 N'est pas un soucis
+    this.x += speed;
   }
 
-  //faut-il fait une function pour eliminer les ships(objets) qui sortent de la fenêtre ?
+  //faut-il fait une pour eliminer les ships(objets) qui sortent de la fenêtre ?
 }
