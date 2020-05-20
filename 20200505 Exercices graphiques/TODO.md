a0284e8cca0bf501b6de597014474473237762e7
Wara-nyu <w.bounkeua@gmail.com>
2020-05-20 09:54:32 +0200

    les étoiles clignotent

--- 20200505 Exercices graphiques/TODO.md
+++ 20200505 Exercices graphiques/TODO.md
@@ -1,7 +1,3 @@
-- faire apparaitre les etoiles tout le long de l'animation
-- étoiles clignotent (couleur/noir)
-- écraser les étoiles dans l'array qui sortent de la fenêtre par de nouvelles étoiles
-
 - creer des vaisseaux de fond plus petit que le vaisseau principal (attention à ce que les petits vaisseaux ne passe pas "devant" le vaisseaux principal)
 (- faire en sortent que les petits vaisseaux ne se passe pas l'un au dessus de l'autre)
 - faire un vaisseaux dans l'autre sens (sideShip)...?
--- 20200505 Exercices graphiques/star.js
+++ 20200505 Exercices graphiques/star.js
@@ -2,7 +2,7 @@ class Star {
     constructor() {
         this.starX = random(w);
         this.starY = random(h);
-        this.starSize = random(10);
+        this.starSize = random(7);
 		this.starColor = this.randomColor();
     }
 
@@ -10,12 +10,14 @@ class Star {
 		return {r: random(255), g: random(255), b: random(255)};
 	}
 
-	starFill(color) {
-	}
-
   create() {
-  fill(this.starColor.r, this.starColor.g, this.starColor.b);
+    let rand = Math.floor((Math.random() * 2));
+    console.log(rand);
+    if(rand === 0) {
+      fill(this.starColor.r, this.starColor.g, this.starColor.b);
       ellipse(this.starX, this.starY, this.starSize);
+    } else {
+    }
   }
 
   sMove() {
@@ -23,6 +25,7 @@ class Star {
   }
 
   asign() {
+    //fill(this.starColor.r, this.starColor.g, this.starColor.b); à clarifier
     this.starX = width +5;
     this.starY = random(h);
   }
