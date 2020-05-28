02b64da2a6c83ea71debaa4fe3bb58ff803f412f
Wara-nyu <w.bounkeua@gmail.com>
2020-05-28 18:24:17 +0200

    encéphalogramme plat

--- 20200505 Exercices graphiques/ECGcourbe/obj.js
+++ 20200505 Exercices graphiques/ECGcourbe/obj.js
@@ -6,15 +6,17 @@ class Obj {
   }
 
   drawObj() {
-    fill(100);
+    fill(0);
     rect(this.x, 0, this.r + 5, h);
     fill(0, 255, 0);
     noStroke();
-    ellipse(this.x, h/2, this.x);
+    ellipse(this.x, h/2, this.r);
   }
 
   move() {
-    if(this.x < w) {
+    if(this.x >= w) {
+      this.x =0;
+    }else{
       this.x += 1;
     }
   }
