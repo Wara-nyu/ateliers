f3136c1b833c481c530a6400d8c636b5f1a33f19
Wara-nyu <w.bounkeua@gmail.com>
2020-05-31 20:47:21 +0200

    pas l'effet attendu : 'point' qui suit une courbe

--- 20200505 Exercices graphiques/ECGcourbe/main.js
+++ 20200505 Exercices graphiques/ECGcourbe/main.js
@@ -1,21 +1,26 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
 let x = 0;
-let r = 5;
+let radius = 75;
+let py;
+let angle;
+let frequence = 5;
 //let obj;
 
 function setup() {
   createCanvas(w, h);
-  obj = new Obj;
+  //obj = new Obj;
   background(0);
 }
 
 function draw(){
+  py = height/2 + sin(radians(angle))*(radius);
   fill(0);
-  rect(x, 0, r+5, h);
+  rect(x, 0, 6, h);
   fill(0,255,0);
   noStroke();
-  ellipse(x, h/2, r);
+  ellipse(10+x, py, 5, 5);
+  angle -= frequence;
   move();
   //obj.drawObj();  
   //obj.move(); 
@@ -24,8 +29,8 @@ function draw(){
 function move() {
   if(x>=w) {
     x=0;
+    angle = 0;
   }else{
-    x+=3;
+    x+=1;
   }
 }
-
